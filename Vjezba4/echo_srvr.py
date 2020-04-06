f264fe804c90800bf07cd1c033eb3874cbed6bda
ivankraljevic177 <ivankraljevic177@gmail.com>
2020-04-06 17:57:21 +0200

    push

--- Vjezba4/echo_srvr.py
+++ Vjezba4/echo_srvr.py
@@ -2,10 +2,14 @@ import socket
 
 host = socket.gethostname()
 port = 12345
+IPAddr = socket.gethostbyname(host)  
 
 echo_server = socket.socket()
 echo_server.bind((host,port))
 echo_server.listen(5)
+
+print (IPAddr)
+
 
 print ("Cekam klijenta...")
 conn, addr = echo_server.accept()
